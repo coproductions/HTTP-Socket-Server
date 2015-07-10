2721dd6bdd8020bf22c3b29bd6bedaf591b8b58e
Constanti <constantin.dumba@gmail.com>
2015-07-09 20:11:20 -1000

    added methods for client

--- client.js
+++ client.js
@@ -1,25 +1,44 @@
 var net = require('net');
 var PORT = 8080;
 var ADDRESS = '0.0.0.0';
+var method = 'GET';
 
 var responseCache = {};
 
 process.stdin.setEncoding('utf8');
-var requestArgument = process.argv[2];
+var requestArgument = process.argv[process.argv.length - 1];
+var requestHost = requestArgument.split('/')[0];
+console.log('host: ',requestHost);
+var requestURI = requestArgument.slice(requestHost.length,requestArgument.length)
+console.log('uri: ',requestURI);
 
 
-if(requestArgument){
-  var requestHost = requestArgument.split('/')[0];
-  console.log('host: ',requestHost);
-  var requestURI = requestArgument.slice(requestHost.length,requestArgument.length)
-  console.log('uri: ',requestURI);
+if(process.argv.length > 3){
+  switch(process.argv[2]){
+    case '-method':
+      method = process.argv[3];
+      break;
+    case '-head' :
+      method = 'HEAD';
+      break;
+    case '-port' :
+      PORT = Number(process.argv[3]);
+      break;
+  }
+
+    var client = net.connect({port: PORT}, connectedToServer);
+  client.setEncoding('utf8');
+
+} else if(process.argv.length > 2){
+
 
   //only connect if there is a request argument
   var client = net.connect({port: PORT}, connectedToServer);
   client.setEncoding('utf8');
 
-} else{
-  process.stdout.write('hello')
+}
+else{
+  process.stdout.write('The following commands are available to you:\n-method: allows you to determine the method. Follow it with a method.\n-head: requests only the head.\n-port: needs to be followed by a port to determine the port.')
   //include held menu here
 }
 
@@ -27,16 +46,17 @@ if(requestArgument){
 function connectedToServer(){
   console.log('client connected')
   client.on('data',responseHandler)
-  var requestHeader = generateHeader(requestURI,requestHost)
+  var requestHeader = generateHeader(requestURI,requestHost,method)
   client.write(requestHeader);
 }
 
-function generateHeader(uri,host){
+function generateHeader(uri,host,method){
   var requestString = '';
-  requestString += 'GET '+uri+' HTTP/1.1'+'\n';
+  requestString += method+''+uri+' HTTP/1.1'+'\n';
   requestString += 'Date: '+new Date().toUTCString()+ '/n';
   requestString += 'Host: '+host + '/n';
   requestString += 'Const Http';
+  console.log('requestString',requestString)
   return requestString;
 }
 
