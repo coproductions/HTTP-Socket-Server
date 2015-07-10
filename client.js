f2dbbfdc2fca48d6e7db075e8f80b4ef0f7bf0ee
Constanti <constantin.dumba@gmail.com>
2015-07-09 15:16:00 -1000

    updated client to cache response

--- client.js
+++ client.js
@@ -2,14 +2,25 @@ var net = require('net');
 var PORT = 8080;
 var ADDRESS = '0.0.0.0';
 
-var client = net.connect({port: PORT}, connectedToServer);
+var responseCache = {};
+
 process.stdin.setEncoding('utf8');
 var requestArgument = process.argv[2];
+
+
 if(requestArgument){
   var requestHost = requestArgument.split('/')[0];
   console.log('host: ',requestHost);
   var requestURI = requestArgument.slice(requestHost.length,requestArgument.length)
   console.log('uri: ',requestURI);
+
+  //only connect if there is a request argument
+  var client = net.connect({port: PORT}, connectedToServer);
+  client.setEncoding('utf8');
+
+} else{
+  process.stdout.write('hello')
+
 }
 
 
@@ -23,10 +34,18 @@ function connectedToServer(){
 function generateHeader(uri,host){
   var requestString = '';
   requestString += 'GET '+uri+' HTTP/1.1'+'\n';
+  requestString += 'Date: '+new Date().toUTCString()+ '/n';
   requestString += 'Host: '+host + '/n';
+  requestString += 'Const Http';
   return requestString;
 }
 
 function responseHandler(chunk){
   process.stdout.write(chunk);
+  console.log('chunk:',chunk)
+  var responseHeader = chunk.split('\n\n')[0];
+  var timeReceived = toString(Date.now());
+  responseCache[timeReceived] = responseHeader;
+  console.log('cache',responseCache)
+
 }
