58252403c854a357b7171746c961da14a7cbc04e
Constanti <constantin.dumba@gmail.com>
2015-07-09 14:42:43 -1000

    client requesting stuff

--- client.js
+++ client.js
@@ -3,7 +3,30 @@ var PORT = 8080;
 var ADDRESS = '0.0.0.0';
 
 var client = net.connect({port: PORT}, connectedToServer);
+process.stdin.setEncoding('utf8');
+var requestArgument = process.argv[2];
+if(requestArgument){
+  var requestHost = requestArgument.split('/')[0];
+  console.log('host: ',requestHost);
+  var requestURI = requestArgument.slice(requestHost.length,requestArgument.length)
+  console.log('uri: ',requestURI);
+}
+
 
 function connectedToServer(){
   console.log('client connected')
+  client.on('data',responseHandler)
+  var requestHeader = generateHeader(requestURI,requestHost)
+  client.write(requestHeader);
+}
+
+function generateHeader(uri,host){
+  var requestString = '';
+  requestString += 'GET '+uri+' HTTP/1.1'+'\n';
+  requestString += 'Host: '+host + '/n';
+  return requestString;
+}
+
+function responseHandler(chunk){
+  process.stdout.write(chunk);
 }
